107357a6d363df247d02609e58eb42a453015bb4
KJcode <kuba10j@gmail.com>
2021-06-07 19:53:25 +0200

    porządki v2

--- Zajęcia/Temat14/src/EmptyPlaylistException.java
+++
@@ -1,6 +0,0 @@
-public class EmptyPlaylistException extends Exception{
-    EmptyPlaylistException() {
-        super("Playlista nie zawiera utworow!");
-        Odtwarzacz.printYellow("Playlista nie zawiera utworow!");
-    }
-}
--- Zajęcia/Temat14/src/UserAbortedException.java
+++
@@ -1,7 +0,0 @@
-
-class UserAbortedException extends Exception {
-    UserAbortedException() {
-        super("Operacja przerwana przez uzytkownika!");
-        Odtwarzacz.printYellow("Operacja przerwana przez uzytkownika!");
-    }
-}
